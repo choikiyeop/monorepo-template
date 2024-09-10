f13095dae3ab797b553e34b2fa25379be79dbdb3
choikiyeop <cky9709@naver.com>
2024-09-10 14:27:37 +0900

    feat(ui): tailwindcss 설정

---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: "selector",
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
