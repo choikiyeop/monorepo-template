5c9300aa38b275a751c72ee53aec148f31eb47ae
choikiyeop <cky9709@naver.com>
2024-09-11 11:28:06 +0900

    feat(web): tailwind 설정

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss';
+import sharedConfig from '@repo/tailwind-config/tailwind.config';
+
+const config: Pick<Config, 'content' | 'presets'> = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    '../../packages/ui/src/**/*.{ts,tsx}',
+  ],
+  presets: [sharedConfig],
+};
+
+export default config;
